0488782d6c5bc60607f870a58884fb9ba1c45797
sm-amoled <moktak072@naver.com>
2023-10-29 15:36:16 +0900

    에니메이션 작동

--- sandbox/lib/home.dart
+++ sandbox/lib/home.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sandbox/clap_button/clap_button_page.dart';
+import 'package:sandbox/emoji_firework_animation/emoji_firework_page.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
 
+  final sandboxList = [
+    (ClapButtonPage(), "Clap Button Animation"),
+    (EmojiFireworkPage(), "Emoji Firework"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                // Navigator.push(context, CupertinoPageRoute(
-                Navigator.push(context, CupertinoPageRoute(
-                  builder: (context) {
-                    return const ClapButtonPage();
-                  },
-                ));
-              },
-              child: const Text("Clap Button"))
-        ],
-      )),
+      appBar: AppBar(title: const Text("SandBox")),
+      body: ListView.builder(
+        itemCount: sandboxList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  // Navigator.push(context, CupertinoPageRoute(
+                  Navigator.push(context, CupertinoPageRoute(
+                    builder: (context) {
+                      return sandboxList[index].$1;
+                    },
+                  ));
+                },
+                child: Text(sandboxList[index].$2)),
+          );
+        },
+      ),
     );
   }
 }
